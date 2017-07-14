aa7615ea74a1fa44cb0d37f76932c607c7d022a8
keven518 <957314724@qq.com>
2017-07-14 11:49:48 +0800

    4-1

--- config/routes.js
+++ config/routes.js
@@ -11,7 +11,9 @@ module.exports = function(app) {
   app.use(function(req, res, next) {
     var _user = req.session.user;
 
+    if(_user) {
       app.locals.user = _user;
+    }
 
       next();
   })
